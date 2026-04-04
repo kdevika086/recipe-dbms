72dbd63ed72960f6bc2df7d578774072fa7ba75f
Devika Kundathil Anoop <devika.kundathil086@outlook.com>
2026-04-05 01:14:00 +0530

    Added actual Render URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 cconst API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000/api"
-    : "https://YOUR-RENDER-URL.onrender.com/api";
+    : "https://recipefinder-8ts9.onrender.com/";
